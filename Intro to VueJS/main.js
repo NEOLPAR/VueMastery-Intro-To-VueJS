c2c294279fbb3d774ebf5e2638d2e3136b6897e5
Ricardo Del Rio Rubial <ricardo@neolpar.com>
2021-02-15 21:52:41 +0000

    Lesson 7

--- Intro to VueJS/main.js
+++ Intro to VueJS/main.js
@@ -1,22 +1,24 @@
 var app = new Vue({
   el: '#app',
   data: {
+    brand: 'Vue Mastery',
     product: 'Socks',
-    image: './assets/vmSocks-green-onWhite.jpg',
-    inStock: false,
-    inventory: 0,
-    onSale: true,
+    selectedVariant: 0,
     details: ['80% Cotton', '20% Polyester', 'Gender-neutral'],
     variants:[
       {
         variantId: 2234,
         variantColor: 'green',
-        variantImage: './assets/vmSocks-green-onWhite.jpg'
+        variantImage: './assets/vmSocks-green-onWhite.jpg',
+        variantQuantity: 5,
+        onSale: true
       },
       {
         variantId: 2235,
         variantColor: 'blue',
-        variantImage: './assets/vmSocks-blue-onWhite.jpg'
+        variantImage: './assets/vmSocks-blue-onWhite.jpg',
+        variantQuantity: 0,
+        onSale: false
       }
     ],
     sizes: [5, 6, 7, 8, 9],
@@ -29,8 +31,24 @@ var app = new Vue({
     addToCart: function () {
       this.cart += 1;
     },
-    updateProduct: function(newVariantImage) {
-      this.image = newVariantImage;
+    updateProduct: function(idx) {
+      this.selectedVariant = idx;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].variantImage;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].variantQuantity;
+    },
+    onSale() {
+      let isOnSale = this.variants[this.selectedVariant].onSale;
+      
+      return this.brand + ' ' + this.product + (isOnSale ? ' On Sale': '');
     }
   }
 })
